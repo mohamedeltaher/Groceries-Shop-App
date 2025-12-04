a389b3679386c60131031d2915cf996f8ae0b5ec
mohamed eltaher <mohamed.ahmedeltaher@gmail.com>
2025-12-04 17:13:51 +0200

    Create initial SwiftUI project and setup repository

---
+++ Groceries Shop App/Groceries Shop App/Groceries_Shop_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Groceries_Shop_AppApp.swift
+//  Groceries Shop App
+//
+//  Created by Mohamed Eltaher  on 04/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Groceries_Shop_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Groceries Shop App/Groceries Shop AppUITests/Groceries_Shop_AppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Groceries_Shop_AppUITestsLaunchTests.swift
+//  Groceries Shop AppUITests
+//
+//  Created by Mohamed Eltaher  on 04/12/2025.
+//
+
+import XCTest
+
+final class Groceries_Shop_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
